3b55843e338fd3d09e77002298c81b8435c7279b
Pugud19 <noerbayu73@gmail.com>
2024-01-13 16:21:57 +0700

    update vue config

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = {
   devServer: {
     proxy: {
-      '/api': {
-        target: 'https://athayarayamadani.co.id/kreditpensiun-api',
+      '^/api': {
+        target: 'https://athayarayamadani.co.id/kreditpensiun-api/',
+        ws : true,
         changeOrigin: true,
         pathRewrite: {
           '^/api': '/kreditpensiun-api/api',
