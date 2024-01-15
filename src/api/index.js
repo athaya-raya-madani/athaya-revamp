9b6115846b505f32f33d39e0f0ab792c541e7924
Pugud19 <noerbayu73@gmail.com>
2024-01-15 16:41:24 +0700

    reset commit

--- src/api/index.js
+++ src/api/index.js
@@ -2,25 +2,12 @@
 import axios from 'axios';
 
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-// axios.defaults.headers.common['Authorization'] = 'Bearer' + localStorage.getItem('token', token);
 
 const Api = axios.create({
     //set default endpoint API prod
-    baseURL: 'https://athayarayamadani.co.id/kreditpensiun-api/'
+    // baseURL: 'https://athayarayamadani.co.id/kreditpensiun-api/'
     // default endpoint API local
-    // baseURL: 'http://localhost:8080/'
+    baseURL: 'http://localhost:8080/'
 }) 
 
-Api.interceptors.request.use(config => {
-    // Modify the headers to include CORS-related headers
-    config.headers['Access-Control-Allow-Origin'] = 'https://beautiful-cajeta-90bf33.netlify.app';
-    config.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS, PUT, DELETE';
-    config.headers['Access-Control-Allow-Headers'] = 'Content-Type';
-    
-    return config;
-  }, error => {
-    // Do something with request error
-    return Promise.reject(error);
-  });
-  
 export default Api
--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 
-module.exports = {
-  devServer: {
-    proxy : 'https://athayarayamadani.co.id/kreditpensiun-api/',
-    // proxy: {
-    //   '^/api': {
-    //     target: 'https://athayarayamadani.co.id/kreditpensiun-api/',
-    //     ws : true,
-    //     changeOrigin: true,
-    //     pathRewrite: {
-    //       '^/api': '/kreditpensiun-api/api',
-    //     },
-    //   },
-    // },
-  },
-};
-
-
+module.exports = defineConfig({
+  publicPath: '/sneat/',
+})
