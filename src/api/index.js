20264ab92264e1eb6b82f53e9813b7c137bba269
Pugud19 <noerbayu73@gmail.com>
2024-01-17 10:21:29 +0700

    update api url

--- src/api/index.js
+++ src/api/index.js
@@ -5,9 +5,9 @@ axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 const Api = axios.create({
     //set default endpoint API prod
-    // baseURL: 'https://athayarayamadani.co.id/kreditpensiun-api/'
+    baseURL: 'https://athayarayamadani.co.id/kreditpensiun-api/'
     // default endpoint API local
-    baseURL: 'http://localhost:8080/'
+    // baseURL: 'http://localhost:8080/'
 }) 
 
 export default Api
