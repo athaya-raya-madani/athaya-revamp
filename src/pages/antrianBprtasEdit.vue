b03e60347ca5b65447f70f124a005ddf6d96a1ea
Pugud19 <noerbayu73@gmail.com>
2024-03-28 09:51:02 +0700

    show dokslikojk file in verif and bank

--- src/pages/antrianBprtasEdit.vue
+++ src/pages/antrianBprtasEdit.vue
@@ -10,6 +10,28 @@ const goBack = () => {
   router.go(-1);
 }
 
+const dokslikojk = async () => {
+  try {
+    const response = await api.get(`/api/permohonan/slik-ojk/${route.params.id}`);
+    const filename = response.data.filename;
+    if (filename) {
+      const filePath = `http://localhost:8080/${filename}`;
+      console.log('File Path:', filePath);
+      window.open(filePath, '_blank');
+    } else {
+      // Show pop-up if filename is null
+      alert('File Tidak Ada');
+    }
+  } catch (error) {
+    if (error.response.status === 404) {
+      // Show pop-up if file not found
+      alert('File Tidak Ditemukan');
+    } else {
+      console.error('Error:', error);
+    }
+  }
+}
+
 const dilanjutkan = async () => {
   try {
     const response = await api.post(`/api/permohonan/${route.params.id}/statusbi-approve`);
@@ -45,6 +67,9 @@ const ditolak = async () => {
       <VCol cols="12">
         <VBtn type="reset" class="my-2" @click="goBack" >
           <i class="mdi icon-lg mdi-arrow-left"></i> Back
+        </VBtn>
+        <VBtn class="btn-verifikasi my-2 mx-1" @click="dokslikojk" >
+          <i class="mdi icon-lg mdi-file-pdf-box"></i> Dokumen Slik
         </VBtn>
         <VBtn class="btn-verifikasi my-2 mx-1" @click="dilanjutkan" >
           <i class="mdi icon-lg mdi-check-circle"></i> Dilanjutkan
