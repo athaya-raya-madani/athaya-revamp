940f47de915bd22497d7ba0b2b1d77c05d86a1ae
Pugud19 <noerbayu73@gmail.com>
2024-04-19 08:25:11 +0700

    revisi wording

--- src/pages/hasil-analisa.vue
+++ src/pages/hasil-analisa.vue
@@ -6,7 +6,7 @@ import IndexData from '@/views/pages/tables/hasilCekBank.vue';
   <VRow>
 
     <VCol cols="12">
-      <VCard title="Data Pemohon">
+      <VCard title="Data Hasil Analisa Bank">
         <VBtn :to="{ name: 'permohonans.create'}" class="ml-5 v-btn btn-primary">
           <i class="mdi icon-lg mdi-plus"></i>Tambah Pemohon
         </VBtn>
